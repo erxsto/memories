90f5bf181b0782c7d86986580c63e2ebf7937c8d
Flores Vazquez Erasto Geovany <al221911907@gmail.com>
2021-07-07 17:39:03 -0500

    arreglos de css

--- resources/views/content/indexf.blade.php
+++ resources/views/content/indexf.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-
+<!--
 <section id="main" class="wrapper style1">
 	<div class="container">
 		<div class="row">
@@ -9,7 +9,7 @@
 				<div class="card">
 					<img src="{{ asset($file->url) }}" alt="" class="img-fluid">
 					<div class="card-footer">
-						 <!--EDITAR <button href="{{ route('editf', $file) }}" class="btn btn-info">Editar</button>-->
+						 EDITAR <button href="{{ route('editf', $file) }}" class="btn btn-info">Editar</button>
 						<form action="{{ route('destroyf', $file) }}" class="d-inline formulario-eliminar" method="POST">
 							@method('DELETE')
 							@csrf
@@ -23,26 +23,23 @@
 		</div>
 	</div>
 	<div class="title">Mis recuerdos:</div>
-</section
-<section id="highlights" class="wrapper style1">
+</section>-->
+<section id="main" class="wrapper style1">
 	<div class="text-center">
-		<select class="selectpicker" name="heredero" id="heredero" >
+		<select class="selectpicker btn-danger" name="heredero" id="heredero">
 			<option value=""> &nbsp;&nbsp;&nbsp;Selecciona un Heredero&nbsp;&nbsp;&nbsp;</option>
 
 			@foreach($herederos as $heredero)
-			<option value="{{$heredero->id_destinatario}}">{{$heredero->nombre}}</option>
+			<option style="background: #ec5353; color: #fff;" value="{{$heredero->id_destinatario}}">{{$heredero->nombre}}</option>
 			@endforeach
 		</select>
 		<div id="recuerdo_h">
 			<br><br><br><br><br><br>
 		</div>
 	</div>
-	</div>
-	</div>
-	<div class="container">
-	</div>
 </section>
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<!--
 @if (session('eliminar') == 'ok')
 <script>
 	Swal.fire(
@@ -74,15 +71,13 @@
 			}
 		})
 	});
-</script>
+</script>-->
 <script>
 	$(function() {
 
 		$('#heredero').on('change', cambiodeh);
 
 	});
-
-
 	function cambiodeh() {
 
 		var h_id = $(this).val();
@@ -94,34 +89,23 @@
 			for (var i = 0; i < data.length; i++) {
 
 				var nombreh = '' + data[0].nombre + ' ' + data[0].app + ' ' + data[0].apm + '';
-				
-				if(data[i].iof == 'i'){
-				html_div += '<div class="col"><div class="p-3 border bg-light"><div class="content"><img src="http://127.0.0.1/memories/public/' + data[i].url + '" alt="http://127.0.0.1/memories/public/' + data[i].url + '" class="img-thumbnail" alt="" width="236" height="236"><br>'+data[i].nombrea+'</div></div><br><button id="eliminarArticulo" onclick="eliminarArticulo('+data[i].id+')" class="btn btn-outline btn-danger"><i class="fa fa-trash"></i></button></div>';
-				}else{
-				html_div += '<div class="col"><div class="p-3 border bg-light"><div class="content"><img src="./images/fileee.png" alt="http://127.0.0.1/memories/public/' + data[i].url + '" class="img-thumbnail" alt="" width="236" height="236"><br>'+data[i].nombrea+'</div></div><br><button id="eliminarArticulo" onclick="eliminarArticulo('+data[i].id+')" class="btn btn-outline btn-danger"><i class="fa fa-trash"></i></button></div>';
+
+				if (data[i].iof == 'i') {
+					html_div += '<div class="col"><div class="p-3 border bg-light"><div class="content"><img src="http://127.0.0.1/memories/public/' + data[i].url + '" alt="http://127.0.0.1/memories/public/' + data[i].url + '" class="img-thumbnail" alt="" width="236" height="236"><br>' + data[i].nombrea + '</div></div><br><button id="eliminarArticulo" onclick="eliminarArticulo(' + data[i].id + ')" class="btn btn-outline btn-danger"><i class="fa fa-trash"></i></button></div>';
+				} else {
+					html_div += '<div class="col"><div class="p-3 border bg-light"><div class="content"><img src="./images/fileee.png" alt="http://127.0.0.1/memories/public/' + data[i].url + '" class="img-thumbnail" alt="" width="236" height="236"><br>' + data[i].nombrea + '</div></div><br><button id="eliminarArticulo" onclick="eliminarArticulo(' + data[i].id + ')" class="btn btn-outline btn-danger"><i class="fa fa-trash"></i></button></div>';
 				}
-				
 				$('#recuerdo_h').html(html_div);
 			}
-			if(nombreh!=undefined){
-			html_div += '</div></div><br><br><div class="alert alert-info" role="alert"> Recuerdos de ' + nombreh + '</div><br><br>';
-			$('#recuerdo_h').html(html_div);
-			}else{
-			$('#recuerdo_h').html('</div></div><br><br><div class="alert alert-dark" role="alert"> No hay recuerdos de éste usuario.</div><br><br>');
-
+			if (nombreh != undefined) {
+				html_div += '</div></div><br><br><div class="alert alert-danger" role="alert"> Recuerdos de ' + nombreh + '</div><br><br>';
+				$('#recuerdo_h').html(html_div);
+			} else {
+				$('#recuerdo_h').html('</div></div><br><br><div class="alert alert-danger" role="alert"> No hay recuerdos de éste usuario.</div><br><br>');
 			}
 		});
-
-
-
-
 	}
-
-	
-
 	function eliminarArticulo(id) {
-		 
-		 
 		Swal.fire({
 			title: '¿Estás seguro?',
 			text: "¡Este recuerdo se eliminará definitivamente!",
@@ -132,30 +116,21 @@
 			confirmButtonText: 'Si, ¡eliminar!',
 			cancelButtonText: 'Cancelar'
 		}).then((result) => {
-				if (result.isConfirmed) {
-
-
-					$.ajax({
-		    		url: '/memories/public/eliminarArticulo/' + id,
-		    		type: 'GET',
-		    		success: function(result) {
-					
+			if (result.isConfirmed) {
+				$.ajax({
+					url: '/memories/public/eliminarArticulo/' + id,
+					type: 'GET',
+					success: function(result) {
 						//  console.log(result);
 						window.location.reload();
 						Swal.fire(
-						'Eliminado!',
-						'El recuerdo se eliminó con éxito.',
-						'success') 
-					
+							'Eliminado!',
+							'El recuerdo se eliminó con éxito.',
+							'success')
 					}
-		 			});
-				}
+				});
+			}
 		})
-		 
-	} 
-
-</script>
-<script>
-
+	}
 </script>
 @endsection
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -39,30 +39,26 @@
 		}
 	</style>
 	<header class="header">
-
 		@include('layouts.menu')
 	</header>
-
-
 	<div id="page-wrapper">
-
 		<!-- Header -->
 		<section id="header" class="wrapper">
-
 			<!-- Logo -->
 			<div id="logo">
 				<h1><a href="{{route('login')}}">MEMORIES</a></h1>
 				<p>Los recuerdos lo son todo</p>
 			</div>
-
 			<!-- Nav -->
 		</section>
 		@yield('appblade')
-		<div style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
-				<path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #ff8000;"></path>
-			</svg></div>
+		<div style="height: 150px; overflow: hidden;">
+			<svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
+				<path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #ff8000;">
+				</path>
+			</svg>
+		</div>
 		<!-- Intro -->
-		</ul>
 	</div>
 	<!-- Script dropzone-->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.2/min/dropzone.min.js"></script>
